e9072982e5bc05724dab7412c6fbbd9dcb2c0e6f
aghamoutsim <aghamoutsim@gmail.com>
2023-05-15 12:50:18 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+//CALCULATER#
+
+var calculater = prompt("Enter your First Number");
+var calculater1 = prompt("Enter your Second Number");
+var operater = prompt("Enter your oprater");
+
+if(operater ==="+"){
+    console.log(calculater + calculater1);
+}
+
+else if(operater === "-"){
+    console,log(calculater - calculater1);
+}
+
+else if(operater === "*"){
+    console,log(calculater * calculater1);
+}
+
+else if(operater === "%"){
+    console.log(calculater % calculater1);
+}
+
+else if(operater === "/"){
+    console.log(calculater / calculater1);
+}
+
+
+
+
+
+
